5bb2d8f1f8f6d700d9656751d7bafb0a5a1bf9c7
siggu <jmkim.flickdone@gmail.com>
2025-06-10 16:55:25 +0900

    fix: basePath 설정

--- next.config.mjs
+++ next.config.mjs
@@ -8,5 +8,6 @@ const withNextra = nextra({
 // Export the final Next.js config with Nextra included
 export default withNextra({
   output: 'export',
+  basePath: '/flickdone-nextra',
   trailingSlash: true,
 });
